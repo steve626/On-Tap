7fd0ace28bb2ec85937f9a926a86af2840236ca5
Steve Valasek <svalasek@gmail.com>
2018-05-31 14:25:24 -0700

    changing beerList to beerlist and pushing to heroku

--- app/models/beerlist.js
+++ app/models/beerlist.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var beerList = sequelize.define('beerList', {
+  var beerlist = sequelize.define('beerlist', {
     brewery_name: DataTypes.STRING,
     city_name: DataTypes.STRING,
     beer_name: DataTypes.STRING,
     beer_type: DataTypes.STRING
   }, {});
-  beerList.associate = function(models) {
+  beerlist.associate = function(models) {
     // associations can be defined here
   };
-  return beerList;
+  return beerlist;
 };
